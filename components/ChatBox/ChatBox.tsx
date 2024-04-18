96f922fd41fb1a625a1f451209cc581b6db35bbb
Jberma23 <Jesse.ben.berman@gmail.com>
2024-04-18 15:39:16 -0600

    changing chatbox to be width-full

--- components/ChatBox/ChatBox.tsx
+++ components/ChatBox/ChatBox.tsx
@@ -4,7 +4,7 @@ export const ChatBox: React.FC<ChatBoxProps> = ({}) => {
   return (
     <div
       id="chat-box"
-      className="block h-40 border border-slate-400 border-solid rounded-md w-96 mx-auto text-black py-3 px-3"
+      className="block h-40 border border-slate-400 border-solid rounded-md w-full mx-auto text-black py-3 px-3"
     ></div>
   );
 };
